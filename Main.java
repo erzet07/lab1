5e51e46e90c933cb66918e685558e8af7d169866
Erik Zetterlund <7anerik@gmail.com>
2023-11-06 11:26:34 +0100

    test123

---
+++ Main.java
@@ -0,0 +1,12 @@
+import org.w3c.dom.ls.LSOutput;
+import java.awt.*;
+import java.sql.SQLOutput;
+import java.util.LinkedList;
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
